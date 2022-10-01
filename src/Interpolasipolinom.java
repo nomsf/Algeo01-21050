2d9665ffd27c46f75b75afe6693447bd653cb970
Dhiwa27 <88771673+Dhiwa27@users.noreply.github.com>
2022-10-01 20:25:53 +0700

    Update Interpolasipolinom.java

--- src/Interpolasipolinom.java
+++ src/Interpolasipolinom.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 public class Interpolasipolinom{ 
+    public static double[][] scanfile(Scanner read)
+    {
+        String namefile; 
+        namefile=IOFile.InputFileName(read);
+        int nR = IOFile.RowCounter(namefile);
+        int nC = IOFile.ColCounter(namefile); 
+        double[][] initmatrix= IOFile.readFile(namefile, nR, nC) ;
+        return initmatrix; 
+    }
     public static double approach(double[] koefisien,double X)
     {
         int nkoef = koefisien.length; 
@@ -63,6 +73,100 @@ public class Interpolasipolinom{
                 }
             } 
         }
+
+    
         return koefisien; 
+    }
+    
+    
+    public static String equation (double[] koefisien,double result)
+    {
+        String reseq ="Persamaan polinomial yang dihasilkan adalah: \nf(x)=  ";
+        boolean nol=true;
+        
+        for (int z = koefisien.length-1;z>=0; z--)
+        {
+            while (nol)
+            { 
+                if (koefisien[z]==0) 
+                {
+                    nol=false; 
+                }
+                else if (koefisien[z]>0 )
+                {
+                    if (z==koefisien.length-1)
+                    { 
+                        reseq+=String.valueOf(koefisien[z]);
+                    }  
+                    else 
+                    {
+                        reseq+= " + "+String.valueOf(koefisien[z]);  
+                    }
+                }
+                else
+                {
+                    reseq+= " - "+String.valueOf(koefisien[z]);      
+                }
+                if (z!=0) 
+                {
+                    reseq+="X^"+String.valueOf(z) ; 
+                }
+            } 
+        }
+        Scanner read = new Scanner(System.in); 
+        read.close();
+        int X = read.nextInt(); 
+        
+        reseq+= "\nf("+String.valueOf(X)+")="+String.valueOf(result); 
+        return reseq; 
+    
+    }
+        
+
+
+    public static void Interpolasipolinomdriver(Scanner read)
+    {
+        Menu.menuInput();
+        int input=IOKeyboard.InputOption(read); 
+        
+        switch(input) {
+            case 1: 
+                double[][] initmatrixkeyboard = IOKeyboard.readMatrix(read); 
+                int x = read.nextInt(); 
+                double[] koefisien= polynomEq(initmatrixkeyboard);
+                double result=approach(koefisien, x);
+                Menu.menuWritetoFile();
+                int inputYT = IOKeyboard.InputOption(read);  
+
+                switch(inputYT) {
+                    case 1: 
+                        String namafile = read.next("masukkan nama file:") ;
+                        String poleq = equation(koefisien, result); 
+                        IOFile.writeFile_1(namafile,poleq);
+                }
+            case 2: 
+                double[][] initmatrixkeyboard1 = scanfile(read); 
+                double[][] initmatrixkeyboard2 = new double[initmatrixkeyboard1.length-1][initmatrixkeyboard1[0].length]; 
+                double x1 =initmatrixkeyboard1[initmatrixkeyboard1.length-1][0]; 
+                for (int i=0;i<initmatrixkeyboard1.length-1;i++ )
+                {
+                    for (int j=0 ; j<initmatrixkeyboard1[0].length;j++)
+                    {
+                        initmatrixkeyboard2[i][j]=initmatrixkeyboard1[i][j]; 
+                        
+                    }
+                }
+                double[] koefisien1= polynomEq(initmatrixkeyboard2);
+                double result1=approach(koefisien1, x1);
+                Menu.menuWritetoFile();
+                int inputYT1 = IOKeyboard.InputOption(read);  
+
+                switch(inputYT1) {
+                    case 1: 
+                        String namafile1 = read.next("masukkan nama file:") ;
+                        String poleq1 = equation(koefisien1, result1); 
+                        IOFile.writeFile_1(namafile1,poleq1);
+                }
+        } 
     }
 }
