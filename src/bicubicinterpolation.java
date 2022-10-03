76c9e89637893f0db88a223a64f4a3c2bad8eff5
Dhiwa27 <88771673+Dhiwa27@users.noreply.github.com>
2022-10-03 23:27:55 +0700

    Update bicubicinterpolation.java

--- src/bicubicinterpolation.java
+++ src/bicubicinterpolation.java
@@ -1,199 +1,124 @@
 import java.util.Scanner;
 
-
-public class Interpolasipolinom{ 
-    
-    public static double approach(double[] koefisien,double X)
-    {
-        int nkoef = koefisien.length; 
-        double hasil=0;
-        for (int i=0; i<nkoef;i++)
-        {
-            
-            hasil+=Math.pow(X, i) *koefisien[i]; 
-            System.out.println(hasil);
-        }
-        return hasil;
-    }
-    public static double[][] matrixinterpol(double[][] firstmatrix)
-    {
-        int nrows = firstmatrix.length;
-        int ncolumns = firstmatrix.length + 1; 
-        int i,j;
-        double[][] matrixinterpolasi = new double[nrows][ncolumns]; 
-        for (i=0;i<nrows;i++) 
-        {
-            for (j=0;j<ncolumns;j++) 
+public class bicubicinterpolation 
+{
+    public static double estimateres (double[] koefisien, Double a , Double b)
+    { 
+     
+        double result = 0;
+        int k =0 ;
+        for(int i=0 ; i<4 ; i ++)
+        { 
+            for (int j=0 ; j<4 ;j++ )
             { 
-                if (j==0) 
-                {
-                    matrixinterpolasi[i][0]=1; 
-                }
-                else if (j==ncolumns-1) 
-                { 
-                    matrixinterpolasi[i][j] = firstmatrix[i][1]; 
-                }
-                else 
-                { 
-                    matrixinterpolasi[i][j]=Math.pow(firstmatrix[i][0], j);
-                }
+                result += ( Math.pow(a, i) ) * (Math.pow(b, j)) * (koefisien[k]); 
+                k++ ;
             }
         }
-        return matrixinterpolasi;
-    }
-    public static double[] polynomEq(double[][] matrix) {
-        
+        return result; 
+    } 
 
-        int nrow = matrix.length;
-        int ncol = matrix.length + 1;
-        double[][] matriksPolinom = new double[nrow][ncol];
-        for (int i = 0; i < nrow; i++) {
-            for (int j = 0; j < ncol; j++) {
-                if (j == (ncol - 1)) {
-                    matriksPolinom[i][j] = matrix[i][1];
-                } else if (j == 0) {
-                    matriksPolinom[i][0] = 1.00;
-                } else {
-                    matriksPolinom[i][j] = Math.pow(matrix[i][0], j);
-                }
-            }
 
-        }
-        
-        double[][] newMatrix = gaussjordan.gaussel(matriksPolinom);
-        
-        double[] koefisien = new double[nrow];
-        for (int i = 0; i < nrow; i++) {
-            koefisien[i] = newMatrix[i][ncol - 1];
-        }
 
-        
-        boolean nul = true;
-        System.out.print("Persamaan polinomial yang dihasilkan\ny =");
-        for (int i = koefisien.length - 1; i >= 0; i--) {
-            if (koefisien[i]!=0) {
-                if (nul)
-                    nul = false;
-                else if (koefisien[i] > 0 && !nul)
-                    System.out.print(" +");
-                    System.out.printf( " %f", koefisien[i]);
-                if (i != 0) {
-                    System.out.printf(" x^%d", i);
-                }
-            }
-        }
-        System.out.println();
-
-        return koefisien;
-
-    }
-    
-    
-    public static String equation (double[] koefisien,double result,double x)
-    {
-        String reseq ="Persamaan polinomial yang dihasilkan adalah: \nf(x)=  ";
-        boolean nol=true;
-        
-        for (int z = koefisien.length-1;z>=0; z--)
+    public static double[][] matriksinit (){ 
+        double[][] newmatrix=new double[16][16]; 
+        for(int y=-1; y<3 ; y++ )
         {
-            while (nol)
+            for(int x=-1;x<3;x++ )
             { 
-                if (koefisien[z]==0) 
-                {
-                    nol=false; 
-                }
-                else if (koefisien[z]>0 )
-                {
-                    if (z==koefisien.length-1)
+                for(int j=0 ;j<4;j++ )
+                { 
+                    for(int i= 0;i<4; i++ )
                     { 
-                        reseq+=String.valueOf(koefisien[z]);
-                    }  
-                    else 
-                    {
-                        reseq+= " + "+String.valueOf(koefisien[z]);  
+                        double a = Math.pow(x, i);
+                        double b = Math.pow(y, j); 
+                        newmatrix[i][j] = a * b; 
+
                     }
                 }
-                else
-                {
-                    reseq+= " - "+String.valueOf(koefisien[z]);      
-                }
-                if (z!=0) 
-                {
-                    reseq+="X^"+String.valueOf(z) ; 
-                }
-            } 
-        }
-        
-        
-        
-        
-        reseq+= "\nf("+String.valueOf(x)+")="+String.valueOf(result); 
-        return reseq; 
-    
-    }
-    
-   
-               
-
-    public static void Interpolasipolinomdriver(Scanner read)
-    {
-        
-        int input=IOKeyboard.InputOption(read); 
-        
-    
-        if (input==1) 
-        {
-            double[][] initmatrixkeyboard = IOKeyboard.readMatrix(read);
-            IOKeyboard.printMatrix(initmatrixkeyboard);
-            
-
-            System.out.print("masukkan nilai x:");
-            
-            
-            double x =read.nextDouble(); 
-            
-            double[] koefisien= Interpolasipolinom.polynomEq(initmatrixkeyboard);
-            
-            double result=approach(koefisien, x);
-            System.out.printf("\nf(%.2f)=%.2f\n",x,result);
-            int inputYT = IOKeyboard.WritetoFileOption(read);  
-
-            if (inputYT==1)  
-            {
-                System.out.print("Masukkan nama file: ");
-                String namafile = read.next() ;
-                String poleq = equation(koefisien, result,x); 
-                IOFile.writeFile_1(namafile,poleq);
             }
         }
-        if(input==2)
+        return newmatrix; 
+    }
+    public static double[] bicubiceq(double[][] startmatrix)
+    {
+        
+        
+       
+        int s = 0 ;
+        
+        
+        double[][] newmatrix= bicubicinterpolation.matriksinit() ; 
+        
+
+        double[][] matrixinverse= InverseGauss.inverseGauss(newmatrix);
+        double[][] matrixvalue = new double[16][1]; 
+        for(int j=0;j<=3;j++)
+        {
+            for(int i=0;i<=3;i++)  {
+                matrixvalue[s][0] = startmatrix[j][i];
+                s++; 
+            }
+        }
+        double[][] matrixkoef = MatrixOp.matrixTimes(matrixinverse, matrixvalue);
+        
+        double[] matrixbicubic = new double[matrixkoef.length]; 
+        for (int m=0 ; m< matrixkoef.length;m++ )
         { 
+            matrixbicubic[m] = matrixkoef[m][0]; 
+        }
+        return matrixbicubic; 
+    }
+    
+    public static void bicubicinterpolationdriver (Scanner read)
+    { 
+        int input=IOKeyboard.InputOption(read); 
+        
+        if(input==1) {
+            double[][] initmatrixkeyboard = IOKeyboard.readMatrix(read); 
+            Double x1 = read.nextDouble(); 
+            Double x2 = read.nextDouble(); 
+            double[] matrikskoef = bicubiceq(initmatrixkeyboard); 
+            Double hasilbicubic=estimateres(matrikskoef, x1, x2); 
+            System.out.print("\n");
+            int inputYT=IOKeyboard.WritetoFileOption(read); 
+
+            if (inputYT==1)
+            {
+ 
+                System.out.print("Masukkan nama file : "); 
+                String namafile = read.next(); 
+                String bicubiceq = "f("+String.valueOf(x1)+","+String.valueOf(x2)+")= " + String.valueOf(hasilbicubic); 
+                IOFile.writeFile_1(namafile, bicubiceq);
+            }
+        } 
+        else if(input== 2) 
+        {
             String fileName = IOFile.InputFileName(read);
             int row = IOFile.RowCounter(fileName);
             int col = IOFile.ColCounter(fileName);
         
-            double[][] initmatrixkeyboard1 = IOFile.readFile(fileName,row,col); 
+            double[][] initmatrixkeyboard1 = IOFile.readFile(fileName,row,col);
             IOKeyboard.printMatrix(initmatrixkeyboard1);
             
-            double x1 =read.nextDouble(); 
-
+            System.out.print("x0 = ");
+            double x0 =read.nextDouble();
+            System.out.print("x01 = ");
+            double x01 =read.nextDouble(); 
             
-            double[] koefisien1= Interpolasipolinom.polynomEq(initmatrixkeyboard1);
-            
-            double result1=approach(koefisien1, x1);
-            System.out.printf("\nf(%f)=%f\n",x1,result1);
+            double[] koefisien1=bicubiceq(initmatrixkeyboard1);
+            double result1=estimateres(koefisien1, x0 ,x01);
+                
             int inputYT1 = IOKeyboard.WritetoFileOption(read);  
-
-
-            if (inputYT1==1)
-            { 
+ 
+            if (inputYT1==1 ) { 
                 String namafile1 = read.next() ;
-                String poleq1 = Interpolasipolinom.equation(koefisien1,result1,x1); 
-                IOFile.writeFile_1(namafile1,poleq1);
-                
-                
+                String bicubiceq1 = "f("+String.valueOf(x0)+","+String.valueOf(x01) +")= " + String.valueOf(result1); 
+                IOFile.writeFile_1(namafile1,bicubiceq1);
+                String bicubiceq = "f("+String.valueOf(x0)+","+String.valueOf(x01)+")= " + String.valueOf(result1); 
+                System.out.println(bicubiceq);
             }
         } 
+    
     }
-}
-
+}     
